e24b3357254fea4b665abefb4b2c4d1675e4b60c
bright.jiang <bright.jiang@infotm.com>
2016-02-21 12:48:59 +0800

    add init list

--- include/libClient.h
+++ include/libClient.h
@@ -3,36 +3,57 @@
 
 #include "stdio.h"
 typedef enum{
-
+	LOGIN_DISTRI_PLAT_STEP1_UDP=0,
+	LOGIN_DISTRI_PLAT_STEP2_UDP =1 ,
+	LOGIN_DISTRI_PLAT_STEP1_TCP =2 ,
+	LOGIN_DISTRI_PLAT_STEP2_TCP =3 ,
+	LOGIN_OPERATE_PLAT_STEP   =4,
+	SOCKET_DATA_HANDLER_STEP  =5,
 
 }HanderFunctionEnum;
 typedef enum{
-	HandlerLoginDistributePlatStep1WithUdp,
-	Handler  ,
-	Handler  ,
-	Handler  ,
-	Handler  ,
-	Handler  ,
-	Handler  ,
+	RET_PLAT_ACK_ERR = -5,
+	RET_TOKEN_DOWN = -4,
+	RET_NOT_MATCH = -3,
+	RET_DNS_FAILED = -2,
+	RET_NO_ACK  = -1,
+	RET_OK = 0,
+	RET_PLAT_RECONNECT = 1,
 }HanderRetEnum;
+#define MAX_RET_HANDLER_NUM  5
+#define MAX_HANDLER_NUM  10
+
+typedef int (*FunctionStep)(void *dat) ;
+
 typedef struct {
-	int retValue;
-	void *handler ;
-}RetHander;
-#define MAX_RET_HANDER_NUM  5
+	int err_code;
+	int hander_index;
+}FunctionRetHander;
+
 typedef struct {
-		void *hander ;
-		RetHander[MAX_RET_HANDER_NUM];
-}FunctionFlowCtrol;
+		int hander_index ;
 
-int login_distribute_plat_step1_udp();
-int login_distribute_plat_step2_udp();
-int login_distribute_plat_step1_tcp();
-int login_distribute_plat_step2_tcp();
+		FunctionRetHander  ret_handler[MAX_RET_HANDLER_NUM];
+}FunctionFlowCtrl;
 
+typedef struct {
+	int handler_num ;
+	int handler_flow_num;
+	//
+	FunctionStep hander_list[MAX_HANDLER_NUM];
+	FunctionFlowCtrl flow_list[MAX_HANDLER_NUM];
+} AppFunctionFlowCtrl;
 
-int login_operation_plat();
-int socket_data_handler_loop();
+extern AppFunctionFlowCtrl app_function_flow_ctrl ;
+
+void app_funcion_flow_ctrl_init(void);
+
+int login_distri_plat_step1_udp(void *dat);
+int login_distri_plat_step2_udp(void *dat);
+int login_distri_plat_step1_tcp(void *dat);
+int login_distri_plat_step2_tcp(void *dat);
+int login_operation_plat(void *dat);
+int socket_data_handler_loop(void *dat);
 
 extern void get_version ();
 
--- libClient/libClient.c
+++ libClient/libClient.c
@@ -1,32 +1,166 @@
 #include "libClient.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+AppFunctionFlowCtrl app_function_flow_ctrl ;
 
 void
 get_version(){
-  printf("$(?????????)\n");
+  printf("ret version 0.1\n");
 }
-int loginDistributePlatStep1WithUdp()
+#define   DEBUG_FLOW_CONTROL  1
+int manual_interactive_flow_control(const char *info){
+		char input[64] ;
+	 int ret = 0 ;
+		printf("please input your choice  << %s >>--> ",info);
+		scanf("%d",&ret);
+		printf("\nyou input value -> %d\n",ret);
+		return ret ;
+}
+int login_distri_plat_step1_udp(void *dat)
 {
-
+	LOG_TRACE("%s\n",__FUNCTION__);
+	int ret = 0;
+#if DEBUG_FLOW_CONTROL == 1
+	ret = manual_interactive_flow_control(__FUNCTION__) ;
+	return ret;
+#endif
+	return ret ;
 }
-int loginDistributePlatStep2WithUdp()
+int login_distri_plat_step2_udp(void *dat)
 {
-
+	LOG_TRACE("%s\n",__FUNCTION__);
+	int ret = 0;
+#if DEBUG_FLOW_CONTROL == 1
+	ret = manual_interactive_flow_control(__FUNCTION__) ;
+	return ret;
+#endif
+	return ret ;
 }
-int loginDistributePlatStep1WithTcp()
+int login_distri_plat_step1_tcp(void *dat)
 {
-
+	LOG_TRACE("%s\n",__FUNCTION__);
+	int ret = 0;
+#if DEBUG_FLOW_CONTROL == 1
+	ret = manual_interactive_flow_control(__FUNCTION__) ;
+	return ret;
+#endif
+	return ret ;
 }
 
-int loginDistributePlatStep2WithTcp()
+int login_distri_plat_step2_tcp(void *dat)
 {
-
+	LOG_TRACE("%s\n",__FUNCTION__);
+	int ret = 0;
+#if DEBUG_FLOW_CONTROL == 1
+	ret = manual_interactive_flow_control(__FUNCTION__) ;
+	return ret;
+#endif
+	return ret ;
 }
 
-int loginOperationPlat(){
-
+int login_operation_plat(void *dat){
+	LOG_TRACE("%s\n",__FUNCTION__);
+	int ret = 0;
+#if DEBUG_FLOW_CONTROL == 1
+	ret = manual_interactive_flow_control(__FUNCTION__) ;
+	return ret;
+#endif
+	return ret ;
 }
-int socketDataHandlerLoop(){
+int socket_data_handler_loop(void *dat) {
+	LOG_TRACE("%s\n", __FUNCTION__);
+	int ret = 0;
+#if DEBUG_FLOW_CONTROL == 1
+	ret = manual_interactive_flow_control(__FUNCTION__);
+	return ret;
+#endif
+	return ret;
+}
+/*
+ *
+ * 	LOGIN_DISTRI_PLAT_STEP1_UDP=0,
+	LOGIN_DISTRI_PLAT_STEP2_UDP =1 ,
+	LOGIN_DISTRI_PLAT_STEP1_TCP =2 ,
+	LOGIN_DISTRI_PLAT_STEP2_TCP =3 ,
+	LOGIN_OPERATE_PLAT   =4,
+	SOCKET_DATA_HANDLER  =5,
+ *
+int login_distri_plat_step1_udp(void *dat);
+int login_distri_plat_step2_udp(void *dat);
+int login_distri_plat_step1_tcp(void *dat);
+int login_distri_plat_step2_tcp(void *dat);
+int login_operation_plat(void *dat);
+int socket_data_handler_loop(void *dat);
+ *
+ */
+void app_funcion_flow_ctrl_init(void){
+	memset(&app_function_flow_ctrl,0,sizeof(AppFunctionFlowCtrl));
+
+	//init all handler list
+	app_function_flow_ctrl.hander_list[LOGIN_DISTRI_PLAT_STEP1_UDP] = login_distri_plat_step1_udp;
+	app_function_flow_ctrl.hander_list[LOGIN_DISTRI_PLAT_STEP2_UDP] = login_distri_plat_step2_udp;
+	app_function_flow_ctrl.hander_list[LOGIN_DISTRI_PLAT_STEP1_TCP] = login_distri_plat_step1_tcp;
+	app_function_flow_ctrl.hander_list[LOGIN_DISTRI_PLAT_STEP2_TCP] = login_distri_plat_step2_tcp;
+	app_function_flow_ctrl.hander_list[LOGIN_OPERATE_PLAT_STEP] 		 = login_operation_plat;
+	app_function_flow_ctrl.hander_list[SOCKET_DATA_HANDLER_STEP] 		 = socket_data_handler_loop;
+
+	app_function_flow_ctrl.handler_num = 6 ;
+	//handler LOGIN_DISTRI_PLAT_STEP1_UDP
+	app_function_flow_ctrl.flow_list[0].hander_index = LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[0].ret_handler[0].err_code =  RET_OK;
+	app_function_flow_ctrl.flow_list[0].ret_handler[0].hander_index =  LOGIN_DISTRI_PLAT_STEP2_UDP;
+	app_function_flow_ctrl.flow_list[0].ret_handler[1].err_code = RET_NO_ACK ;
+	app_function_flow_ctrl.flow_list[0].ret_handler[1].hander_index =  LOGIN_DISTRI_PLAT_STEP1_TCP;
+	app_function_flow_ctrl.flow_list[0].ret_handler[2].err_code =  RET_DNS_FAILED;
+	app_function_flow_ctrl.flow_list[0].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_TCP;
+	//handler LOGIN_DISTRI_PLAT_STEP2_UDP
+	app_function_flow_ctrl.flow_list[1].hander_index = LOGIN_DISTRI_PLAT_STEP2_UDP;
+	app_function_flow_ctrl.flow_list[1].ret_handler[0].err_code =  RET_OK;
+	app_function_flow_ctrl.flow_list[1].ret_handler[0].hander_index =  LOGIN_OPERATE_PLAT_STEP;
+	app_function_flow_ctrl.flow_list[1].ret_handler[1].err_code = RET_NO_ACK ;
+	app_function_flow_ctrl.flow_list[1].ret_handler[1].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[1].ret_handler[2].err_code =  RET_DNS_FAILED;
+	app_function_flow_ctrl.flow_list[1].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[1].ret_handler[2].err_code =  RET_NOT_MATCH;
+	app_function_flow_ctrl.flow_list[1].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	//handler LOGIN_DISTRI_PLAT_STEP1_TCP
+	app_function_flow_ctrl.flow_list[2].hander_index = LOGIN_DISTRI_PLAT_STEP1_TCP;
+	app_function_flow_ctrl.flow_list[2].ret_handler[0].err_code =  RET_OK;
+	app_function_flow_ctrl.flow_list[2].ret_handler[0].hander_index =  LOGIN_DISTRI_PLAT_STEP2_TCP;
+	app_function_flow_ctrl.flow_list[2].ret_handler[1].err_code = RET_NO_ACK ;
+	app_function_flow_ctrl.flow_list[2].ret_handler[1].hander_index =  LOGIN_DISTRI_PLAT_STEP1_TCP;
+	app_function_flow_ctrl.flow_list[2].ret_handler[2].err_code =  RET_DNS_FAILED;
+	app_function_flow_ctrl.flow_list[2].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_TCP;
+	//handler LOGIN_DISTRI_PLAT_STEP2_TCP
+	app_function_flow_ctrl.flow_list[3].hander_index = LOGIN_DISTRI_PLAT_STEP2_TCP;
+	app_function_flow_ctrl.flow_list[3].ret_handler[0].err_code =  RET_OK;
+	app_function_flow_ctrl.flow_list[3].ret_handler[0].hander_index =  LOGIN_OPERATE_PLAT_STEP;
+	app_function_flow_ctrl.flow_list[3].ret_handler[1].err_code = RET_NO_ACK ;
+	app_function_flow_ctrl.flow_list[3].ret_handler[1].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[3].ret_handler[2].err_code =  RET_DNS_FAILED;
+	app_function_flow_ctrl.flow_list[3].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+
+	//handler LOGIN_OPERATE_PLAT_STEP
+	app_function_flow_ctrl.flow_list[4].hander_index = LOGIN_DISTRI_PLAT_STEP2_UDP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[0].err_code =  RET_OK;
+	app_function_flow_ctrl.flow_list[4].ret_handler[0].hander_index =  SOCKET_DATA_HANDLER_STEP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[1].err_code = RET_NO_ACK ;
+	app_function_flow_ctrl.flow_list[4].ret_handler[1].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[2].err_code =  RET_DNS_FAILED;
+	app_function_flow_ctrl.flow_list[4].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	//handler SOCKET_DATA_HANDLER_STEP
+	app_function_flow_ctrl.flow_list[4].hander_index = LOGIN_DISTRI_PLAT_STEP2_UDP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[0].err_code =  RET_OK; //never reach here
+	app_function_flow_ctrl.flow_list[4].ret_handler[0].hander_index =  SOCKET_DATA_HANDLER_STEP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[1].err_code = RET_NO_ACK ;
+	app_function_flow_ctrl.flow_list[4].ret_handler[1].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[2].err_code =  RET_TOKEN_DOWN;
+	app_function_flow_ctrl.flow_list[4].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[2].err_code =  RET_PLAT_ACK_ERR;
+	app_function_flow_ctrl.flow_list[4].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
+	app_function_flow_ctrl.flow_list[4].ret_handler[2].err_code =  RET_PLAT_RECONNECT;
+	app_function_flow_ctrl.flow_list[4].ret_handler[3].hander_index =  LOGIN_DISTRI_PLAT_STEP1_UDP;
 
 }
 
